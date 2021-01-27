08fc93017141e8835597f2076a4c5071b68f37b6
zhaochengyong <zhaochengyong126@126.com>
2021-01-27 10:27:54 +0800

    练习文件 IO

---
+++ src/main/java/com/tengyun/java/jdk/io/FileIoTest.java
@@ -0,0 +1,10 @@
+package com.tengyun.java.jdk.io;
+
+/**
+ * @author Adam Zhao
+ */
+public class FileIoTest {
+    public static void main(String[] args) {
+        System.out.println("main method");
+    }
+}
