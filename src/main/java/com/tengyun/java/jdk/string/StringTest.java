227f6b7ea2695423e5c25d85a2d09e8552c80681
zhaochengyong <zhaochengyong126@126.com>
2021-01-27 10:28:13 +0800

    练习 String

---
+++ src/main/java/com/tengyun/java/jdk/string/StringTest.java
@@ -0,0 +1,10 @@
+package com.tengyun.java.jdk.string;
+
+/**
+ * @author Adam Zhao
+ */
+public class StringTest {
+    public static void main(String[] args) {
+        System.out.println("Test string");
+    }
+}
