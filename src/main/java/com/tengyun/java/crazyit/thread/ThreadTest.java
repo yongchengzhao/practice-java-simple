40eff5649ef6de6ff851af9dc315cea68985542c
zhaochengyong <zhaochengyong126@126.com>
2021-04-16 14:14:52 +0800

    继承 Thread 类创建线程。

---
+++ src/main/java/com/tengyun/java/crazyit/thread/ThreadTest.java
@@ -0,0 +1,38 @@
+package com.tengyun.java.crazyit.thread;
+
+/**
+ * @author Adam Zhao
+ */
+public class ThreadTest {
+    public static void main(String[] args) {
+        System.out.println("===================Thread Test===============");
+
+        // 创建线程
+        MyThread thread1 = new MyThread("thread1");
+        MyThread thread2 = new MyThread("thread2");
+        MyThread thread3 = new MyThread("thread3");
+
+        // 启动线程
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
+
+class MyThread extends Thread {
+
+    public MyThread() {}
+
+    public MyThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        // run() 方法的方法体执行任务代码
+        int time = 100;
+        for (int i = 0; i < time; i++){
+            System.out.println(this.getName() + "-------------");
+        }
+    }
+}
