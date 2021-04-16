98e26d4c03bf96b8b84720913ca39f1f256afde7
zhaochengyong <zhaochengyong126@126.com>
2021-04-16 16:13:10 +0800

    练习创建 UUID。

---
+++ src/main/java/com/tengyun/java/jdk/uuid/UUIDTest.java
@@ -0,0 +1,23 @@
+package com.tengyun.java.jdk.uuid;
+
+import java.util.UUID;
+
+/**
+ * @author Adam Zhao
+ */
+public class UUIDTest {
+    public static void main(String[] args) {
+        createUUID();
+    }
+
+    public static void createUUID() {
+        UUID u1 = UUID.randomUUID();
+        UUID u2 = UUID.randomUUID();
+
+        // u1: 3ca5f128-5536-47b9-85fe-9020cb397f8c
+        System.out.println("u1: " + u1);
+        // u2: 8961cd7b-3c19-40cc-a1eb-e4d26ea751d6
+        System.out.println("u2: " + u2);
+        System.out.println(u1.compareTo(u2));
+    }
+}
