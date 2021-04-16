5fa5e6867583e6f8de3106e120b46563a7683555
zhaochengyong <zhaochengyong126@126.com>
2021-04-16 16:05:26 +0800

    练习字符串的比较。

--- src/main/java/com/tengyun/java/jdk/string/StringTest.java
+++ src/main/java/com/tengyun/java/jdk/string/StringTest.java
@@ -6,5 +6,17 @@ package com.tengyun.java.jdk.string;
 public class StringTest {
     public static void main(String[] args) {
         System.out.println("Test string");
+
+        testCompare();
+    }
+
+    public static void testCompare() {
+        String s1 = "God";
+        String s2 = "Adam";
+        String s3 = "Eve";
+
+        // s1.compareTo(s2) 就相当于 s1 - s2
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s3.compareTo(s2));
     }
 }
