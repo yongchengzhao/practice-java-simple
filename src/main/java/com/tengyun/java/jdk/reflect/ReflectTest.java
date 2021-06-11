6cbd2a701486c44a736b70838635af3e9a2e9d7c
zhaochengyong <zhaochengyong126@126.com>
2021-06-11 14:04:28 +0800

    使用反射调用方法。

--- src/main/java/com/tengyun/java/jdk/reflect/ReflectTest.java
+++ src/main/java/com/tengyun/java/jdk/reflect/ReflectTest.java
@@ -2,13 +2,15 @@ package com.tengyun.java.jdk.reflect;
 
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * @author Adam Zhao
  */
 public class ReflectTest {
-    public static void main(String[] args) throws NoSuchFieldException, ClassNotFoundException, IllegalAccessException {
+    public static void main(String[] args) throws Exception {
         getFieldVal();
+        invokeMethod();
     }
 
     public static void getFieldVal() throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
@@ -17,5 +19,15 @@ public class ReflectTest {
 
         System.out.println(pi.get(null));
         System.out.println(pi.get(mathClass));
+    }
+
+    public static void invokeMethod() throws Exception {
+        Method abs = Math.class.getDeclaredMethod("abs", double.class);
+        double douVal = (double) abs.invoke(null, -3.14);
+        System.out.println(douVal);
+
+        Method pow = Math.class.getDeclaredMethod("pow", double.class, double.class);
+        double powResult = (double) pow.invoke(null, 2, 3);
+        System.out.println(powResult);
     }
 }
