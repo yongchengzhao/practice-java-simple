49bf6297e06779bdd215018c5e01f25a64c660ed
zhaochengyong <zhaochengyong126@126.com>
2021-06-11 14:21:20 +0800

    联系字符串的比较。

--- src/main/java/com/tengyun/java/jdk/string/StringTest.java
+++ src/main/java/com/tengyun/java/jdk/string/StringTest.java
@@ -16,7 +16,9 @@ public class StringTest {
         String s3 = "Eve";
 
         // s1.compareTo(s2) 就相当于 s1 - s2
-        System.out.println(s1.compareTo(s2));
-        System.out.println(s3.compareTo(s2));
+        // 6，相当于 G - A = 6
+        System.out.println("s1.compareTo(s2): " + s1.compareTo(s2));
+        // 4，相当于 E - A = 4
+        System.out.println("s3.compareTo(s2): " + s3.compareTo(s2));
     }
 }
