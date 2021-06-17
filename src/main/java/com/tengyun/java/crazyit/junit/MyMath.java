6437b40d38cafe18615ef2e26cd3dcdd52ce66c7
zhaochengyong <zhaochengyong126@126.com>
2021-06-17 20:02:59 +0800

    练习 junit5 单元测试框架。

---
+++ src/main/java/com/tengyun/java/crazyit/junit/MyMath.java
@@ -0,0 +1,11 @@
+package com.tengyun.java.crazyit.junit;
+
+/**
+ * @author Adam Zhao
+ */
+public class MyMath {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/main/java/com/tengyun/java/crazyit/junit/MyMathTest.java
@@ -0,0 +1,7 @@
+package com.tengyun.java.crazyit.junit;
+
+/**
+ * @author Adam Zhao
+ */
+public class MyMathTest {
+}
