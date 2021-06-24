68353efa228d9468686c7db308f45e0994972e1c
zhaochengyong <zhaochengyong126@126.com>
2021-06-24 09:01:29 +0800

    练习对象克隆。

---
+++ src/main/java/com/tengyun/java/jdk/clone/CloneTest.java
@@ -0,0 +1,35 @@
+package com.tengyun.java.jdk.clone;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+
+/**
+ * @author Adam Zhao
+ */
+@Data
+@AllArgsConstructor
+public class CloneTest implements Cloneable {
+
+    private String name;
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    public static void test1() {
+        CloneTest ct1 = new CloneTest("Adam");
+        System.out.println("ct1: " + ct1);
+        System.out.println("ct1 identity hash code: " + System.identityHashCode(ct1));
+        System.out.println("ct1 hash code: " + ct1.hashCode());
+
+        try {
+            CloneTest ct2 = (CloneTest) ct1.clone();
+            System.out.println("ct2: " + ct2);
+            System.out.println("ct2 identity hash code: " + System.identityHashCode(ct2));
+            System.out.println("ct2 hash code: " + ct2.hashCode());
+        } catch (CloneNotSupportedException e) {
+            System.out.println("不允许克隆");
+        }
+    }
+}
